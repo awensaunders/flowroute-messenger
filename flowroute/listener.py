1c856069bbcc4af10e46ffd875a7e9487f60fddb
Awen Saunders <awensaunders@users.noreply.github.com>
2016-07-02 01:23:30 +0100

    Update listener.py to be compatible with mod_wsgi

--- flowroute/listener.py
+++ flowroute/listener.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 import flowroute.flowroute
 app = Flask(__name__)
 
-
+msg = flowroute.flowroute.Messages()
 text = "Hi! If you're looking for Awen, he has moved to the UK. His new phone number is +44 (0) 7455230843. Please do not reply to this automated message."
 
 @app.route('/',  methods=['POST'])
@@ -14,10 +14,9 @@ def post_run():
     
     
 def main():
-    msg = flowroute.flowroute.Messages()
+#    msg = flowroute.flowroute.Messages()
     app.run(
         host="0.0.0.0",
         port=int("1997")
     )    
 if __name__ == '__main__':
-    main()
